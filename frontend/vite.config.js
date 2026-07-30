7beeb7cec6294e59f9da5b6b58fb85f330a23bbf
Jasjeev Singh <jasjeev99@gmail.com>
2026-07-30 20:12:03 +0000

    Dockerize the application

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    host: true, // This exposes the server to 0.0.0.0
+    port: 5173,
+    watch: {
+      usePolling: true, // Needed for hot-reloading inside Docker Windows/Mac
+    }
+  }
 })
